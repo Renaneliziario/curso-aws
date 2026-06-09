6305d22e65e8e15d488729290dd7e4103d78ffce
Renan Queiroz Eliziario <renanqueiroz92@gmail.com>
2026-06-09 20:08:55 -0300

    Spring boot hello world

---
+++ src/main/java/com/renan/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package com.renan.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World - AWS EC2!";
+    }
+
+}
